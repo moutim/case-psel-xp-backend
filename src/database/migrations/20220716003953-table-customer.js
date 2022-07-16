5e72338f08373ddfb3b1ef452da6a7d9b97e985f
unknown <moutimg@gmail.com>
2022-07-15 22:13:44 -0300

    feat: cria arquivo de migration do Sequelize para criacao da tabela 'customer'

---
+++ src/database/migrations/20220716003953-table-customer.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('customer', {
+      customerId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      lastName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      balance: {
+        allowNull: false,
+        type: Sequelize.DECIMAL
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('customer');
+  }
+};
