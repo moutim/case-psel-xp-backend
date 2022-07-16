a45b70a61fe54da6a97ec8f82bdce76fb8f720ad
unknown <moutimg@gmail.com>
2022-07-16 04:41:19 -0300

    feat: cria arquivo na camada de model para a tabela 'customerStockWallet'

---
+++ src/database/models/customer.stock.wallet.js
@@ -0,0 +1,32 @@
+const walletCustomer = (sequelize, DataTypes) => {
+  const walletCustomerTable = sequelize.define("customerStockWallet", 
+  {
+    stockId: DataTypes.INTEGER,
+    customerId: DataTypes.INTEGER,
+    quantity: DataTypes.INTEGER,
+    value: DataTypes.DECIMAL
+  },
+  { timestamps: false }
+  );
+
+  walletCustomerTable.associate = (models) => {
+    models.customer.belongsToMany(models.stock, {
+      through: walletCustomerTable,
+      foreignKey: 'customerId',
+      otherKey: 'stockId',
+      as: 'stock'
+    });
+
+    models.stock.belongsToMany(models.customer, {
+      through: walletCustomerTable,
+      foreignKey: 'stockId',
+      otherKey: 'customerId',
+      as: 'customer'
+    });
+  }
+
+  return walletCustomerTable;
+}
+
+module.exports = walletCustomer;
+
