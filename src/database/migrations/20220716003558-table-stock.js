1b20f0746e8f3abc600e0273b809dbb6956c4cd1
unknown <moutimg@gmail.com>
2022-07-15 22:13:30 -0300

    feat: cria arquivo de migration do Sequelize para criacao da tabela 'stock'

---
+++ src/database/migrations/20220716003558-table-stock.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('stock', {
+      stockId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      value: {
+        allowNull: false,
+        type: Sequelize.DECIMAL
+      },
+      quantity: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      companyId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'company',
+          key: 'companyId'
+        },
+        onDelete: 'CASCADE',
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('stock');
+  }
+};
