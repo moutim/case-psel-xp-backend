2a15dedc4f9772559f1e7f1bb837c26b52b38180
unknown <moutimg@gmail.com>
2022-07-15 21:26:20 -0300

    feat: inicia sequelize

---
+++ src/database/config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.HOSTNAME,
+    dialect: process.env.DIALECT,
+  },
+  test: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.HOSTNAME,
+    dialect: process.env.DIALECT,
+  },
+  production: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.HOSTNAME,
+    dialect: process.env.DIALECT,
+  },
+};
