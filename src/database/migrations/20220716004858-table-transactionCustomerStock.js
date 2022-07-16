720d55f01b5605d8a627e4126a66174a78bc1b8a
unknown <moutimg@gmail.com>
2022-07-15 22:15:03 -0300

    feat: cria arquivo de migration do Sequelize para criacao da tabela 'transactionCustomerStock'

---
+++ src/database/migrations/20220716004858-table-transactionCustomerStock.js
@@ -0,0 +1,53 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('transactionCustomerStock', {
+      stockId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'stock',
+          key: 'stockId'
+        },
+        onDelete: 'CASCADE',
+        primaryKey: true,
+      },
+      customerId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'customer',
+          key: 'customerId'
+        },
+        onDelete: 'CASCADE',
+        primaryKey: true,
+      },
+      value: {
+        allowNull: false,
+        type: Sequelize.DECIMAL
+      },
+      quantity: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      date: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now')
+      },
+      typeId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'transactionType',
+          key: 'typeId'
+        },
+        onDelete: 'CASCADE',
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('transactionCustomerStock');
+  }
+};
