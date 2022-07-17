e21908074cbfea81a5c4ef7b189682a9e9b16a61
unknown <moutimg@gmail.com>
2022-07-17 19:23:03 -0300

    refactor: refatora funcoes do JWT

--- src/utils/JWT.js
+++ src/utils/JWT.js
@@ -1,21 +1,24 @@
 const { StatusCodes } = require('http-status-codes');
 const jwt = require('jsonwebtoken');
 
-const { SECRET } = process.env;
-
 const jwtConfig = {
   expiresIn: '8h',
   algorithm: 'HS256',
 };
 
-const generateToken = (payload) => jwt.sign(payload, SECRET, jwtConfig);
+const generateToken = (payload) => jwt.sign(payload, process.env.SECRET, jwtConfig);
 
 const authenticateToken = (token) => {
-  const result = jwt.verify(token, SECRET, jwtConfig);
+  if (!token) {
+    throw Object({ status: StatusCodes.UNAUTHORIZED, message: '"Token" is required' });
+  }
 
-  if (result) return result;
-
-  throw Object({ status: StatusCodes.UNAUTHORIZED, message: 'Invalid token' });
+  try {
+    const payload = jwt.verify(token, process.env.SECRET, jwtConfig);
+    return payload;
+  } catch (e) {
+    throw Object({ status: StatusCodes.UNAUTHORIZED, message: 'Invalid token' });
+  }
 };
 
 module.exports = {
