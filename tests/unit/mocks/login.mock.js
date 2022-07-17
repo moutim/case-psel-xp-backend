bea02d20c710e9f956f8a9df71071da6c2805294
unknown <moutimg@gmail.com>
2022-07-17 19:55:12 -0300

    tests: refatora tests de login

--- tests/unit/mocks/login.mock.js
+++ tests/unit/mocks/login.mock.js
@@ -13,7 +13,13 @@ const returnFindOne = {
   },
 };
 
+const emailNotFound = { status: 404, message: 'Email not found' };
+
+const incorrectPassword = { status: 401, message: 'Incorrect password' };
+
 module.exports = {
   resultLogin,
   returnFindOne,
+  emailNotFound,
+  incorrectPassword,
 };
