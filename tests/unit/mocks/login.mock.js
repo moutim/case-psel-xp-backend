782e31dc90e5ac1ded2bc8a3fe9c6c5aa98605ce
unknown <moutimg@gmail.com>
2022-07-17 03:37:49 -0300

    tests: cria mocks para a camada controller de login

---
+++ tests/unit/mocks/login.mock.js
@@ -0,0 +1,24 @@
+const resultLogin = {
+  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdXN0b21lcklkIjoxLCJpYXQiOjE2NTgwMzQ4ODcsImV4cCI6MTY1ODA2MzY4N30.Yl63GEqGPL5QKxEiM_9yP_U2KB2gBR3yShMBSC0orfo',
+  customerId: 1,
+  firstName: 'Vitor',
+  lastName: 'Moutim',
+  email: 'moutimg@gmail.com',
+  balance: '150',
+};
+
+const returnFindOne = {
+  dataValues: {
+    customerId: 1,
+    firstName: 'Vitor',
+    lastName: 'Moutim',
+    email: 'moutimg@gmail.com',
+    password: 'password',
+    balance: '150',
+  },
+};
+
+module.exports = {
+  resultLogin,
+  returnFindOne,
+};
