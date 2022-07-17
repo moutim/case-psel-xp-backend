08435778e71c02631cbd29fcbff432e86213a35e
unknown <moutimg@gmail.com>
2022-07-17 19:38:21 -0300

    feat: cria arquivo de mocks para o customer

---
+++ tests/unit/mocks/customer.mock.js
@@ -0,0 +1,11 @@
+const resultGetCustomerInfos = {
+  customerId: 1,
+  firstName: 'Vitor',
+  lastName: 'Moutim',
+  email: 'moutimg@gmail.com',
+  balance: '150',
+};
+
+module.exports = {
+  resultGetCustomerInfos,
+};
