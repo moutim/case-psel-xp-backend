7cfaaec9f3b535223bfea69a503060a6cb7e2950
unknown <moutimg@gmail.com>
2022-07-17 01:47:43 -0300

    refactor: insere senha criptografada no banco

--- src/database/seeders/20220716075533-seeder-customer.js
+++ src/database/seeders/20220716075533-seeder-customer.js
@@ -7,14 +7,14 @@ module.exports = {
         firstName: 'Vitor',
         lastName: 'Moutim',
         email: 'moutimg@gmail.com',
-        password: 'password',
+        password: '$2b$10$wYeJ4kRrLrrQK1hboCBh4uTeq.Vm9kgLgpNlWA/pW5/l6VRCz.xMq',
         balance: 150
       },
       {
         firstName: 'Silvania',
         lastName: 'Moutim',
         email: 'silvania_moutim@gmail.com',
-        password: 'password',
+        password: '$2b$10$wYeJ4kRrLrrQK1hboCBh4uTeq.Vm9kgLgpNlWA/pW5/l6VRCz.xMq',
         balance: 400
       },
     ], {});
