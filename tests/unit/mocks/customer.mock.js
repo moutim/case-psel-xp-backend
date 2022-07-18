8ef58ae8e137667bcb17a5e61a7b1cac42648fe0
unknown <moutimg@gmail.com>
2022-07-18 02:27:38 -0300

    tests: adiciona mocks para o customer

--- tests/unit/mocks/customer.mock.js
+++ tests/unit/mocks/customer.mock.js
@@ -3,7 +3,7 @@ const resultGetCustomerInfos = {
   firstName: 'Vitor',
   lastName: 'Moutim',
   email: 'moutimg@gmail.com',
-  balance: '150',
+  balance: 150,
 };
 
 const customerNotFound = { status: 404, message: 'Customer with id 1 not found' };
@@ -12,9 +12,18 @@ const informationUpdated = { message: 'Information updated successfully' };
 
 const informationNotUpdated = { status: 500, message: 'There was an error updating the information' };
 
+const withdrawSuccessful = { message: 'Withdrawal successful' };
+
+const withdrawError = { status: 500, message: 'An error occurred while performing the transaction' };
+
+const insufficientBalance = { status: 401, message: 'Insufficient balance for this transaction' };
+
 module.exports = {
   resultGetCustomerInfos,
   customerNotFound,
   informationUpdated,
   informationNotUpdated,
+  withdrawSuccessful,
+  withdrawError,
+  insufficientBalance,
 };
