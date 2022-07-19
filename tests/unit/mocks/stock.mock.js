9d72436424d0876fdbf59574b1e841937c6b96fe
unknown <moutimg@gmail.com>
2022-07-18 21:30:24 -0300

    tests: cria mocks para stocks

--- tests/unit/mocks/stock.mock.js
+++ tests/unit/mocks/stock.mock.js
@@ -23,7 +23,28 @@ const stocks = [
 
 const stocksNotFound = { status: 404, message: 'There are no stocks available at the moment' };
 
+const stockNotFound = { status: 404, message: 'Stock with id 1 not found' };
+
+const buyMade = {
+  message: 'Successful purchase',
+  transactionId: 10,
+};
+
+const insufficientStocks = { status: 401, message: 'There are not enough stocks to make the purchase' };
+
+const insufficientBalance = { status: 401, message: 'Insufficient balance for this transaction' };
+
+const customerStockWallet = { quantity: 10, value: 100 };
+
+const transactionError = { status: 500, message: 'An error occurred while performing the transaction' };
+
 module.exports = {
   stocks,
   stocksNotFound,
+  buyMade,
+  stockNotFound,
+  insufficientStocks,
+  insufficientBalance,
+  customerStockWallet,
+  transactionError,
 };
