fb9fc02c47a7bfff126f73d40a3ac4b8cb085137
unknown <moutimg@gmail.com>
2022-07-19 02:18:57 -0300

    fix: remove comentario perdido

--- src/routes/register.routes.js
+++ src/routes/register.routes.js
@@ -7,8 +7,5 @@ const controller = require('../controllers/register.controller');
 const middlewares = require('../middlewares');
 
 routes.post('/', middlewares.verifyRegister, controller.newRegister);
-// routes.post('/', (req, res) => {
-//   res.status(200).json({ message: 'PASSSO' });
-// });
 
 module.exports = routes;
