3033089c7c53c2395c06458bf93e6de3d3d2abbe
unknown <moutimg@gmail.com>
2022-07-19 00:12:25 -0300

    feat: cria rota POST '/register' para registar novo cliente

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,8 @@ const middlewares = require('../middlewares');
 
 routes.use('/login', require('./login.routes'));
 
+routes.use('/register', require('./register.routes'));
+
 routes.use('/customer', middlewares.authenticateToken, require('./customer.routes'));
 
 routes.use('/stock', middlewares.authenticateToken, require('./stock.routes'));
