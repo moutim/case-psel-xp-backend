588ba7467c20c1b601185036d82d160eb18e9084
unknown <moutimg@gmail.com>
2022-07-21 03:27:41 -0300

    feat: cria rota GET '/stocks/:id' para listar acao pelo id

--- src/routes/stock.routes.js
+++ src/routes/stock.routes.js
@@ -6,6 +6,8 @@ const controller = require('../controllers/stock.controller');
 
 routes.get('/', controller.getStocks);
 
+routes.get('/:id', controller.getStockById);
+
 routes.post('/buy', controller.buyStocks);
 
 routes.post('/sell', controller.sellStocks);
