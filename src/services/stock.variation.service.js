2d2492dd9701c3a1d6697304d472861988c9eee8
unknown <moutimg@gmail.com>
2022-07-23 00:49:19 -0300

    refactor: muda nome de variaveis

--- src/services/stock.variation.service.js
+++ src/services/stock.variation.service.js
@@ -6,10 +6,6 @@ const config = require('../database/config/config');
 const sequelize = new Sequelize(config.development);
 
 const variationStock = async () => {
-  // Cria variavel para colocar situacao da funcao pois o forEach nao retorna nada
-  // let functionSituation;
-
-  // Encontra todas as ações
   const stocks = await stock.findAll();
 
   // Passa por todas as ações
@@ -18,38 +14,36 @@ const variationStock = async () => {
     let newPrice;
 
     // Número flutuante aleatório de 0 a 4 que determinará qual a porcentagem a ser calculada
-    const percentage = parseFloat((Math.random() * 4).toFixed(2));
+    const variationPercentage = parseFloat((Math.random() * 4).toFixed(2));
 
     // Número inteiro aleatório de 0 a 99 que determinará se soma ou subtrai
-    const addOrSubtract = Math.floor(Math.random() * 100);
+    const randomNumber = Math.floor(Math.random() * 100);
 
     // Valor total a ser somado ou subtraido a partir da porcentagem e do valor atual da ação
-    const valueToBeCalculed = ((value * percentage) / 100);
+    const valueToBeCalculed = ((value * variationPercentage) / 100);
 
     // Subtrai valor total do valor da ação
 
     // Se o numero aleatorio gerador for maior que 50 o valor é somado se nao é subtraido
-    if (addOrSubtract > 49) {
+    if (randomNumber > 49) {
       newPrice = value + valueToBeCalculed;
       oneForUpTwoForDown = 1;
     } else newPrice = value - valueToBeCalculed;
 
     const transaction = await sequelize.transaction(async (t) => {
-      // Atualiza preço da ação na tabela
       const updateStock = await stock.update(
         { value: newPrice },
         { where: { stockId } },
         { transaction: t },
       );
 
-      // Verifica se ação já possui registro na tabela
       const stockAlreadyHasVariation = await stockVariation.findOne({ where: { stockId } });
+
       if (stockAlreadyHasVariation) {
-        // Se tiver, apenas altera os valores
         const updateStockVariation = await stockVariation.update(
           {
             typeId: oneForUpTwoForDown,
-            percentage,
+            percentage: variationPercentage,
             oldPrice: value,
             newPrice: parseFloat(newPrice.toFixed(2)),
             high: parseFloat(newPrice.toFixed(2)) > stockAlreadyHasVariation.dataValues.high
@@ -68,12 +62,11 @@ const variationStock = async () => {
         return false;
       }
 
-      // Se não tiver registro, cria novo registro com os valores
       const createVariationRegister = await stockVariation.create(
         {
           stockId,
           typeId: oneForUpTwoForDown,
-          percentage,
+          percentage: variationPercentage,
           oldPrice: value,
           newPrice: parseFloat(newPrice.toFixed(2)),
           high: parseFloat(newPrice.toFixed(2)),
@@ -87,8 +80,7 @@ const variationStock = async () => {
       return false;
     });
 
-    if (transaction) return false;
-    return false;
+    return transaction;
   });
 
   if (resultUpdateStocks) return 'Stocks price updated successfully';
